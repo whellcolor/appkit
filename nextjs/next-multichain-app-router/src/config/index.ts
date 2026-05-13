26d3f1b14fe0d76d3fc01c78efebb4dd9924f2a2
whellcolor <bahansatekambing@gmail.com>
2026-05-13 22:26:35 +0700

    Update index.ts

--- nextjs/next-multichain-app-router/src/config/index.ts
+++ nextjs/next-multichain-app-router/src/config/index.ts
@@ -4,7 +4,7 @@ import { mainnet, arbitrum, solana, solanaDevnet, solanaTestnet } from '@reown/a
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
 // Get projectId from https://dashboard.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "5022875ce6ee68917f103ff9b1e3422d" // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
