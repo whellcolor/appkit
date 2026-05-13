4a603e363c1779310325e12f25dc7f55b9b23fa3
whellcolor <bahansatekambing@gmail.com>
2026-05-13 22:24:51 +0700

    Update index.ts

--- nextjs/next-core-app-router/src/config/index.ts
+++ nextjs/next-core-app-router/src/config/index.ts
@@ -2,7 +2,7 @@ import { sepolia, mainnet } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
 // Get projectId from https://dashboard.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "5022875ce6ee68917f103ff9b1e3422d" // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
