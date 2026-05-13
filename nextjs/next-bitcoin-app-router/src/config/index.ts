c818c74d7dfb0d82283c196c6b9e8b4d73423de3
whellcolor <bahansatekambing@gmail.com>
2026-05-13 22:22:23 +0700

    Update index.ts

--- nextjs/next-bitcoin-app-router/src/config/index.ts
+++ nextjs/next-bitcoin-app-router/src/config/index.ts
@@ -3,7 +3,7 @@ import type { AppKitNetwork } from '@reown/appkit/networks'
 import { BitcoinAdapter } from '@reown/appkit-adapter-bitcoin'
 
 // Get projectId from https://dashboard.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "5022875ce6ee68917f103ff9b1e3422d" // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
